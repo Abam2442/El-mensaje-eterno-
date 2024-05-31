d7981c72edf0a2233734622a50da326ba3606e41
shahawi <shahawi273@gmail.com>
2024-05-31 06:12:52 +0300

    removed translations from ui added onclick but only to beginning of surah

--- lib/features/quran/presentation/screens/quran_screen.dart
+++ lib/features/quran/presentation/screens/quran_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
-import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:get/get.dart';
 
+import '../../../../core/constants/app_pages_routes.dart';
 import '../../../../core/utils/components/appbar/build_sliver_appbar.dart';
 import '../controller/quran_controller.dart';
 import '../widgets/soura_select_sliver_context.dart';
@@ -23,6 +23,7 @@ class QuranScreen extends GetView<QuranController> {
                 ? SliverList(
                     delegate: SliverChildBuilderDelegate(
                       (context, index) => SearchResultWidget(
+                        index: index,
                         searchResult: controller.searchResults[index],
                       ),
                       childCount: controller.searchResults.length,
@@ -38,61 +39,69 @@ class QuranScreen extends GetView<QuranController> {
 
 class SearchResultWidget extends StatelessWidget {
   final Map<String, dynamic> searchResult;
+  final int index;
 
-  SearchResultWidget({required this.searchResult});
+  SearchResultWidget({required this.searchResult, required this.index});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              searchResult['sora'],
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.blueAccent,
+    return InkWell(
+      onTap: () {
+        Get.find<QuranController>().currentAyat =
+            Get.find<QuranController>().surahs[index].ayat;
+        Get.toNamed(AppPagesRoutes.surahScreen);
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        elevation: 5,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                searchResult['sora'],
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueAccent,
+                ),
               ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              searchResult['ayat'].arabic,
-              style: TextStyle(
-                fontSize: 18,
-                fontStyle: FontStyle.italic,
-                color: Colors.black,
+              SizedBox(height: 10),
+              Text(
+                searchResult['ayat'].arabic,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black,
+                ),
               ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              searchResult['ayat'].julioCortes,
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.deepPurple,
-              ),
-            ),
-            SizedBox(height: 5),
-            Text(
-              searchResult['ayat'].raulGonzalezBornez,
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.green,
-              ),
-            ),
-            SizedBox(height: 5),
-            Text(
-              searchResult['ayat'].muhammadIsaGarcia,
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.teal,
-              ),
-            ),
-          ],
+              // SizedBox(height: 10),
+              // Text(
+              //   searchResult['ayat'].julioCortes,
+              //   style: TextStyle(
+              //     fontSize: 16,
+              //     color: Colors.deepPurple,
+              //   ),
+              // ),
+              // SizedBox(height: 5),
+              // Text(
+              //   searchResult['ayat'].raulGonzalezBornez,
+              //   style: TextStyle(
+              //     fontSize: 16,
+              //     color: Colors.green,
+              //   ),
+              // ),
+              // SizedBox(height: 5),
+              // Text(
+              //   searchResult['ayat'].muhammadIsaGarcia,
+              //   style: TextStyle(
+              //     fontSize: 16,
+              //     color: Colors.teal,
+              //   ),
+              // ),
+            ],
+          ),
         ),
       ),
     );
