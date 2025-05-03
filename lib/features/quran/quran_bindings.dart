f46119e4355fdb3ab8d74821667d4e66d08e32e9
fady mohamed abo alhassan <132343355+Fady4Mohamed@users.noreply.github.com>
2025-05-03 21:37:50 +0300

    return audio remote source file

--- lib/features/quran/quran_bindings.dart
+++ lib/features/quran/quran_bindings.dart
@@ -1,22 +1,32 @@
 import 'package:get/get.dart';
 import 'package:hiwayda_oracion_islamica/core/helpers/network_info.dart';
+import 'package:hiwayda_oracion_islamica/core/services/api_service.dart';
 import 'package:hiwayda_oracion_islamica/core/services/shared_preferences_service.dart';
 import 'package:hiwayda_oracion_islamica/features/quran/data/data_sources/quran_local_data_source.dart';
 import 'package:hiwayda_oracion_islamica/features/quran/data/repository/quran_repo_impl.dart';
 import 'package:hiwayda_oracion_islamica/features/quran/domain/repository/quran_repo.dart';
 import 'package:hiwayda_oracion_islamica/features/quran/presentation/controller/quran_controller.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:hiwayda_oracion_islamica/features/quran/data/data_sources/ayat_audio_remote_source.dart';
+import 'package:http/http.dart' as http;
 
 class QuranBindings extends Bindings {
   @override
   dependencies() async {
     Get.put(InternetConnectionChecker.createInstance());
     Get.put(SharedPreferencesService(pref: Get.find()));
-
     Get.put<NetworkInfo>(NetworkInfoImpl(Get.find()));
-    
+    Get.put(
+      ApiService(
+        client: http.Client(),
+        networkInfo: Get.find(),
+      ),
+    );
     Get.put<QuranLocalDataSource>(
       QuranLocalDataSourceImpl(),
+    );
+    Get.put<AyatAudioRemoteDataSource>(
+      AyatAudioRemoteDataSourceImpl(apiService: Get.find()),
     );
     Get.put<QuranRepo>(
       QuranRepoImpl(
@@ -25,7 +35,5 @@ class QuranBindings extends Bindings {
     );
 
     Get.put(QuranController());
-    // Get.put(AudioService());
-    // Get.put(SurahController());
   }
 }
