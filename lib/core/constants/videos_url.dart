3c25c9ada87fb7d055e122e105a5a1d5f65c5c3f
fady mohamed abo alhassan <132343355+Fady4Mohamed@users.noreply.github.com>
2025-07-19 06:43:30 +0300

    feat: Add new video URLs and enhance URL retrieval functionality

--- lib/core/constants/videos_url.dart
+++ lib/core/constants/videos_url.dart
@@ -1,106 +1,202 @@
 class VideosUrl {
-  static const String aljalsa = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881655/Aljalsa_sceqjz.mp4';
-  static const String allahuakbar = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881656/Allahuakbar_wn3lqg.mp4';
-  static const String anf = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881656/Anf_oairaj.mp4';
-  static const String asr1 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881656/asr1_ngiguy.mp4';
-  static const String asr2 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881656/asr2_vzzuhh.mp4';
-  static const String asr3 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881658/asr3_qz5tnw.mp4';
-  static const String ay = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881658/Ay_wbvug7.mp4';
-  static const String ayah0 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881658/Ayah0_fezqxn.mp4';
-  static const String ayah1 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881659/Ayah1_ehkasc.mp4';
-  static const String ayah2 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881659/Ayah2_oh6yl4.mp4';
-  static const String ayah3 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881659/Ayah3_apkqid.mp4';
-  static const String ayah4 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881660/Ayah4_deqvtf.mp4';
-  static const String ayah5 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881661/Ayah5_uurmyg.mp4';
-  static const String ayah6 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881663/Ayah6_amde3a.mp4';
-  static const String ayah7 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881678/Ayah7_uhauhw.mp4';
-  static const String fam = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881675/Fam_ula0es.mp4';
-  static const String istiftah = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881676/istiftah_sd6uju.mp4';
-  static const String kafayn = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881676/Kafayn_a28ysc.mp4';
-  static const String key = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881676/Key_yaiz9c.mp4';
-  static const String las1 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881676/las1_fpac1q.mp4';
-  static const String las2 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881677/las2_jo4u6l.mp4';
-  static const String las3 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881678/las3_qwtjak.mp4';
-  static const String las4 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881678/las4_a5iugy.mp4';
-  static const String ozon = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881678/Ozon_vaddsm.mp4';
-  static const String qa = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881678/Qa_mm7nrf.mp4';
-  static const String qadam = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881680/Qadam_lmpmuv.mp4';
-  static const String qer2a = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881679/qer2a_mbhubh.mp4';
-  static const String qeyam_men_sujood = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881680/Qeyam_men_sujood_st79dc.mp4';
-  static const String qeyam_men_tachahod = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881681/Qeyam_men_tachahod_loqprm.mp4';
-  static const String qf = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881682/Qf_k0atlk.mp4';
-  static const String qi = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881683/Qi_tx2h24.mp4';
-  static const String rabana_walakalhamd = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881684/Rabana_walakalhamd_qapgp6.mp4';
-  static const String rabi_ighfer_li = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881684/Rabi_ighfer_li_wkmrzp.mp4';
-  static const String raf3_men_roko3 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881684/Raf3_men_roko3_xnrjzi.mp4';
-  static const String ras = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881685/Ras_rckcom.mp4';
-  static const String roko3 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881685/Roko3_hfgg4s.mp4';
-  static const String sami3aallahulemanhamidah = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881686/Sami3aallahulemanhamidah_rvsv3h.mp4';
-  static const String subhanarabiaalaala = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881687/Subhanarabiaalaala_eymnka.mp4';
-  static const String subhanatabiaaladhim = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881688/Subhanatabiaaladhim_hkgexd.mp4';
-  static const String sujood1 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881688/Sujood1_eq2buh.mp4';
-  static const String sujood2 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881688/Sujood2_ewjrv6.mp4';
-  static const String tach = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881694/Tach_zjca4w.mp4';
-  static const String tachahod = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881689/Tachahod_njbkzq.mp4';
-  static const String takbir_ihram = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881689/Takbir_ihram_t4at83.mp4';
-  static const String taslim = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881690/Taslim_xhqsvq.mp4';
-  static const String taslim1 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881691/Taslim1_xn2fmz.mp4';
-  static const String v1 = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881693/v1_kvlxpn.mp4';
-  static const String wajh = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881693/Wajh_ir67v2.mp4';
-  static const String yadayn = 'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881694/Yadayn_vj5emo.mp4';
-
-static String? getUrlByName(String name) {
-  final Map<String, String> urls = {
-    'aljalsa': aljalsa,
-    'allahuakbar': allahuakbar,
-    'anf': anf,
-    'asr1': asr1,
-    'asr2': asr2,
-    'asr3': asr3,
-    'ay': ay,
-    'ayah0': ayah0,
-    'ayah1': ayah1,
-    'ayah2': ayah2,
-    'ayah3': ayah3,
-    'ayah4': ayah4,
-    'ayah5': ayah5,
-    'ayah6': ayah6,
-    'ayah7': ayah7,
-    'fam': fam,
-    'istiftah': istiftah,
-    'kafayn': kafayn,
-    'key': key,
-    'las1': las1,
-    'las2': las2,
-    'las3': las3,
-    'las4': las4,
-    'ozon': ozon,
-    'qa': qa,
-    'qadam': qadam,
-    'qer2a': qer2a,
-    'qeyam_men_sujood': qeyam_men_sujood,
-    'qeyam_men_tachahod': qeyam_men_tachahod,
-    'qf': qf,
-    'qi': qi,
-    'rabana_walakalhamd': rabana_walakalhamd,
-    'rabi_ighfer_li': rabi_ighfer_li,
-    'raf3_men_roko3': raf3_men_roko3,
-    'ras': ras,
-    'roko3': roko3,
-    'sami3aallahulemanhamidah': sami3aallahulemanhamidah,
-    'subhanarabiaalaala': subhanarabiaalaala,
-    'subhanatabiaaladhim': subhanatabiaaladhim,
-    'sujood1': sujood1,
-    'sujood2': sujood2,
-    'tach': tach,
-    'tachahod': tachahod,
-    'takbir_ihram': takbir_ihram,
-    'taslim': taslim,
-    'taslim1': taslim1,
-    'v1': v1,
-    'wajh': wajh,
-    'yadayn': yadayn,
-  };
-  return urls[name];
-}
+  static const String aljalsa =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881655/Aljalsa_sceqjz.mp4';
+  static const String allahuakbar =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881656/Allahuakbar_wn3lqg.mp4';
+  static const String anf =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881656/Anf_oairaj.mp4';
+  static const String asr1 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881656/asr1_ngiguy.mp4';
+  static const String asr2 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881656/asr2_vzzuhh.mp4';
+  static const String asr3 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881658/asr3_qz5tnw.mp4';
+  static const String ay =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881658/Ay_wbvug7.mp4';
+  static const String ayah0 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881658/Ayah0_fezqxn.mp4';
+  static const String ayah1 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881659/Ayah1_ehkasc.mp4';
+  static const String ayah2 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881659/Ayah2_oh6yl4.mp4';
+  static const String ayah3 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881659/Ayah3_apkqid.mp4';
+  static const String ayah4 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881660/Ayah4_deqvtf.mp4';
+  static const String ayah5 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881661/Ayah5_uurmyg.mp4';
+  static const String ayah6 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881663/Ayah6_amde3a.mp4';
+  static const String ayah7 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881678/Ayah7_uhauhw.mp4';
+  static const String fam =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881675/Fam_ula0es.mp4';
+  static const String istiftah =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881676/istiftah_sd6uju.mp4';
+  static const String kafayn =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881676/Kafayn_a28ysc.mp4';
+  static const String key =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881676/Key_yaiz9c.mp4';
+  static const String las1 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881676/las1_fpac1q.mp4';
+  static const String las2 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881677/las2_jo4u6l.mp4';
+  static const String las3 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881678/las3_qwtjak.mp4';
+  static const String las4 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881678/las4_a5iugy.mp4';
+  static const String ozon =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881678/Ozon_vaddsm.mp4';
+  static const String qa =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881678/Qa_mm7nrf.mp4';
+  static const String qadam =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881680/Qadam_lmpmuv.mp4';
+  static const String qer2a =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881679/qer2a_mbhubh.mp4';
+  static const String qeyam_men_sujood =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881680/Qeyam_men_sujood_st79dc.mp4';
+  static const String qeyam_men_tachahod =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881681/Qeyam_men_tachahod_loqprm.mp4';
+  static const String qf =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881682/Qf_k0atlk.mp4';
+  static const String qi =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881683/Qi_tx2h24.mp4';
+  static const String rabana_walakalhamd =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881684/Rabana_walakalhamd_qapgp6.mp4';
+  static const String rabi_ighfer_li =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881684/Rabi_ighfer_li_wkmrzp.mp4';
+  static const String raf3_men_roko3 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881684/Raf3_men_roko3_xnrjzi.mp4';
+  static const String ras =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881685/Ras_rckcom.mp4';
+  static const String roko3 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881685/Roko3_hfgg4s.mp4';
+  static const String sami3aallahulemanhamidah =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881686/Sami3aallahulemanhamidah_rvsv3h.mp4';
+  static const String subhanarabiaalaala =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881687/Subhanarabiaalaala_eymnka.mp4';
+  static const String subhanatabiaaladhim =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881688/Subhanatabiaaladhim_hkgexd.mp4';
+  static const String sujood1 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881688/Sujood1_eq2buh.mp4';
+  static const String sujood2 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881688/Sujood2_ewjrv6.mp4';
+  static const String tach =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881694/Tach_zjca4w.mp4';
+  static const String tachahod =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881689/Tachahod_njbkzq.mp4';
+  static const String takbir_ihram =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881689/Takbir_ihram_t4at83.mp4';
+  static const String taslim =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881690/Taslim_xhqsvq.mp4';
+  static const String taslim1 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881691/Taslim1_xn2fmz.mp4';
+  static const String v1 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881693/v1_kvlxpn.mp4';
+  static const String wajh =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881693/Wajh_ir67v2.mp4';
+  static const String yadayn =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752881694/Yadayn_vj5emo.mp4';
+  static const String mh_1sujood =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752896255/MH-1sujood_ag7nss.mp4';
+  static const String mh_2sujood =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752896233/MH-2sujood_mhvdr8.mp4';
+  static const String mh_fateha1 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752896245/MH-fateha1_lasbqr.mp4';
+  static const String mh_fateha3 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752896248/MH-fateha3_py2pnq.mp4';
+  static const String mh_ihram =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752896227/MH-ihram_aflghx.mp4';
+  static const String mh_ijlas =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752896236/MH-ijlas_wzj51r.mp4';
+  static const String mh_jalsa =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752896245/MH-jalsa_v95rwa.mp4';
+  static const String mh_qyam12 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752896242/MH-qyam12_i9hj44.mp4';
+  static const String mh_qyam23 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752896247/MH-qyam23_b8ngpd.mp4';
+  static const String mh_qyam34 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752896249/MH-qyam34_wowben.mp4';
+  static const String mh_raf3 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752896250/MH-raf3_wrr42b.mp4';
+  static const String mh_rukoo =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752896257/MH-rukoo_v5f6lx.mp4';
+  static const String mh_sujood34 =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752896253/MH-sujood34_l6afnn.mp4';
+  static const String mh_tashahod =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752896259/MH-tashahod_gq4keu.mp4';
+  static const String mh_taslim =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752896253/MH-taslim_ffs9as.mp4';
+  static const String mh_wudoo =
+      'https://res.cloudinary.com/dzdzz2trg/video/upload/v1752896261/MH-Wudoo_d8cypx.mp4';
+  static String? getUrlByName(String name) {
+    final Map<String, String> urls = {
+      'mh-1sujood': mh_1sujood,
+      'mh-2sujood': mh_2sujood,
+      'mh-fateha1': mh_fateha1,
+      'mh-fateha3': mh_fateha3,
+      'mh-ihram': mh_ihram,
+      'mh-ijlas': mh_ijlas,
+      'mh-jalsa': mh_jalsa,
+      'mh-qyam12': mh_qyam12,
+      'mh-qyam23': mh_qyam23,
+      'mh-qyam34': mh_qyam34,
+      'mh-raf3': mh_raf3,
+      'mh-rukoo': mh_rukoo,
+      'mh-sujood34': mh_sujood34,
+      'mh-tashahod': mh_tashahod,
+      'mh-taslim': mh_taslim,
+      'mh-wudoo': mh_wudoo,
+      'aljalsa': aljalsa,
+      'allahuakbar': allahuakbar,
+      'anf': anf,
+      'asr1': asr1,
+      'asr2': asr2,
+      'asr3': asr3,
+      'ay': ay,
+      'ayah0': ayah0,
+      'ayah1': ayah1,
+      'ayah2': ayah2,
+      'ayah3': ayah3,
+      'ayah4': ayah4,
+      'ayah5': ayah5,
+      'ayah6': ayah6,
+      'ayah7': ayah7,
+      'fam': fam,
+      'istiftah': istiftah,
+      'kafayn': kafayn,
+      'key': key,
+      'las1': las1,
+      'las2': las2,
+      'las3': las3,
+      'las4': las4,
+      'ozon': ozon,
+      'qa': qa,
+      'qadam': qadam,
+      'qer2a': qer2a,
+      'qeyam_men_sujood': qeyam_men_sujood,
+      'qeyam_men_tachahod': qeyam_men_tachahod,
+      'qf': qf,
+      'qi': qi,
+      'rabana_walakalhamd': rabana_walakalhamd,
+      'rabi_ighfer_li': rabi_ighfer_li,
+      'raf3_men_roko3': raf3_men_roko3,
+      'ras': ras,
+      'roko3': roko3,
+      'sami3aallahulemanhamidah': sami3aallahulemanhamidah,
+      'subhanarabiaalaala': subhanarabiaalaala,
+      'subhanatabiaaladhim': subhanatabiaaladhim,
+      'sujood1': sujood1,
+      'sujood2': sujood2,
+      'tach': tach,
+      'tachahod': tachahod,
+      'takbir_ihram': takbir_ihram,
+      'taslim': taslim,
+      'taslim1': taslim1,
+      'v1': v1,
+      'wajh': wajh,
+      'yadayn': yadayn,
+    };
+    return urls[name];
+  }
 }
